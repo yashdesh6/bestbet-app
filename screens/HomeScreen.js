3726dc1c2b55ce647299d67d0afabfe9ddd458d3
ShaiGK <38193478+ShaiGK@users.noreply.github.com>
2024-04-28 19:09:04 -0400

    Some changes and progress

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,83 +1,99 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, ScrollView, Image } from 'react-native';
-import NewBetModal from '../components/NewBetModal'; // Adjust the import path if necessary
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+  Image,
+} from "react-native";
+import NewBetModal from "../components/NewBetModal"; // Adjust the import path if necessary
 
 //TODO: Get parlays from SQLite DB
 const parlayData = [
   {
-    title: 'GSW WINS',
-    grade: 'A+',
-    description: 'Pokem ipsum dolor sit amet Croconaw Baltoy Bug Linoone...',
-    image: require('../assets/images/placeholder.png'), // Make sure you have this placeholder image in your assets
+    title: "GSW WINS",
+    grade: "A+",
+    description: "Pokem ipsum dolor sit amet Croconaw Baltoy Bug Linoone...",
+    image: require("../assets/images/placeholder.png"), // Make sure you have this placeholder image in your assets
   },
   {
-    title: 'LEBRON JAMES',
-    grade: 'B-',
-    description: 'Mineral Badge Dugtrio Dragon Rage Manectric Jumpluff Abomasnow...',
-    image: require('../assets/images/placeholder.png'),
+    title: "LEBRON JAMES",
+    grade: "B-",
+    description:
+      "Mineral Badge Dugtrio Dragon Rage Manectric Jumpluff Abomasnow...",
+    image: require("../assets/images/placeholder.png"),
   },
   {
-    title: 'STEPHEN CURRY',
-    grade: 'C-',
-    description: 'Pika-pi Thundershock Parasect deserunt mollit Leech Seed...',
-    image: require('../assets/images/placeholder.png'),
+    title: "STEPHEN CURRY",
+    grade: "C-",
+    description: "Pika-pi Thundershock Parasect deserunt mollit Leech Seed...",
+    image: require("../assets/images/placeholder.png"),
   },
 ];
 
-
-const HomeScreen = ({navigation}) => {
+const HomeScreen = ({ navigation }) => {
   const [modalVisible, setModalVisible] = useState(false);
+  const username = 'SHAI';
 
   return (
     <View className="bg-zinc-950 flex-1">
       <ScrollView>
         <View className="px-4 py-10">
-          //TODO: name integration
-          <Text className="text-white text-2xl font-extrabold my-6">WELCOME, JOHNNY!</Text>
-          <Text className="text-gray-300 mb-4">Let's get started on your next bet!</Text>
-          
-          <Text className="text-blue-500 text-lg font-bold mb-4">EVALUATE PREVIOUS PARLAYS</Text>
+          {/* //TODO: name integration */}
+          <Text className="text-white text-2xl font-extrabold my-6">
+            WELCOME, {username}!
+          </Text>
+          <Text className="text-gray-300 mb-4">
+            Let's get started on your next bet!
+          </Text>
+
+          <Text className="text-blue-500 text-lg font-bold mb-4">
+            EVALUATE PREVIOUS PARLAYS
+          </Text>
           <TextInput
             className="bg-gray-800 text-white rounded-lg px-4 py-2 mb-4"
             placeholder="Search Parlay"
             placeholderTextColor="#d3d3d3"
           />
           {parlayData.map((parlay, index) => (
-            <View key={index} className="bg-gray-800 rounded-lg p-4 mb-4 flex-row items-center">
-              <Image source={parlay.image} className="h-12 w-12 rounded-full mr-4" />
+            <TouchableOpacity
+              onPress={() => navigation.navigate('ParlayDetailScreen')}
+              key={index}
+              className="bg-gray-800 rounded-lg p-4 mb-4 flex-row items-center"
+            >
+              <Image
+                source={parlay.image}
+                className="h-12 w-12 rounded-full mr-4"
+              />
               <View className="flex-1">
                 <Text className="text-white text-lg">{`${parlay.title}: (${parlay.grade})`}</Text>
                 <Text className="text-gray-400">{parlay.description}</Text>
               </View>
-
-            </View>
+            </TouchableOpacity>
           ))}
-
         </View>
       </ScrollView>
-      <TouchableOpacity 
-              onPress={() => setModalVisible(true)}
-              className="bg-blue-500 py-3 px-4 rounded-lg items-center m-4 absolute bottom-0 right-0 left-0 z-10"
-              >
-              <Text className="text-white text-center">New Parlay +</Text>
-          </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => setModalVisible(true)}
+        className="bg-blue-500 py-3 px-4 rounded-lg items-center m-4 absolute bottom-0 right-0 left-0 z-10"
+      >
+        <Text className="text-white text-center">New Parlay +</Text>
+      </TouchableOpacity>
       <NewBetModal
         isVisible={modalVisible}
         onClose={() => setModalVisible(false)}
         // You may want to handle the evaluated bet data here or in the modal component
         onEvaluateBet={(betData) => {
-          console.log('Bet evaluated:', betData);
+          console.log("Bet evaluated:", betData);
           // Perform actions after evaluating the bet, such as updating state
           // Then close the modal
           setModalVisible(false);
-        }
-      }
-      navigation={navigation}
+        }}
+        navigation={navigation}
       />
     </View>
   );
 };
 
 export default HomeScreen;
-
-
--- screens/bets/BetDetailScreen.js
+++ screens/bets/BetDetailScreen.js
@@ -1,37 +1,74 @@
-import React from 'react';
-import { View, Text, ScrollView, Image } from 'react-native';
+import React from "react";
+import { View, Text, ScrollView, Image } from "react-native";
+import { BarChart } from "react-native-gifted-charts";
+import dummyData from "../../dummy.json";
 
 const BetDetailScreen = () => {
   const betData = {
-    name: 'STEPHEN CURRY',
-    strength: 'A+',
-    description: 'Stephen Curry to score over 27.5 points against the Heat',
-    image: require('../../assets/images/placeholder.png'), // Update with the correct path to your image
+    image: require("../../assets/images/placeholder.png"), // Update with the correct path to your image
   };
+
+  const threshold = 15;
 
   return (
     <ScrollView className="bg-zinc-950 flex-1">
       <View className="px-4 py-10 items-center">
         <Image source={betData.image} className="h-24 w-24 rounded-full mb-4" />
-        <Text className="text-white text-3xl font-bold mb-2">{betData.name}</Text>
-        <Text className="text-white text-lg mb-4">Point guard for the Golden State Warriors</Text>
+        <Text className="text-white text-3xl font-bold mb-2">
+          {dummyData.player_name}
+        </Text>
+        <Text className="text-white text-lg mb-4">
+          {dummyData.player_description}
+        </Text>
 
         <View className="w-full bg-gray-700 rounded-lg p-4 mb-6">
           <Text className="text-lg font-bold mb-2 text-white">YOUR BET</Text>
-          <Text className="text-base mb-4 text-white">{betData.description}.</Text>
-          <Text className="text-green-500 font-bold">STRENGTH: {betData.strength}</Text>
+          <Text className="text-base mb-4 text-white">
+            {dummyData.user_bet}.
+          </Text>
+          <Text className="text-green-500 font-bold">
+            STRENGTH: {dummyData.bet_rating}
+          </Text>
         </View>
 
         <View className="w-full bg-gray-700 rounded-lg p-4">
           <Text className="text-lg font-bold mb-2 text-white">WE SUGGEST</Text>
-          <Text className="text-base text-white">
-            With the analysis showing the Heat in strong form and with significant advantages, the risk-to-reward ratio looks favorable
-          </Text>
+          <Text className="text-base text-white">{dummyData.suggestion}</Text>
         </View>
 
         <Text className="text-white text-xl font-bold mt-10 mb-4">INFO</Text>
-        <Text className="text-white text-xl font-bold mb-4">STATISTICS</Text>
-        <Text className="text-white text-xl font-bold">Learn More About The Opposing Team</Text>
+        <Text className="text-white text-l font-bold mb-4">FACTS</Text>
+        {dummyData.player_facts.map((fact, index) => (
+          <View className="w-full bg-gray-700 rounded-lg p-4 mb-4" key={index}>
+            <Text className="text-white text-l font-bold mb-4">{fact}</Text>
+          </View>
+        ))}
+        <Text className="text-white text-l font-bold mb-4">STATISTICS</Text>
+        <View className="w-full bg-gray-700 rounded-lg p-4 mb-4">
+          <BarChart
+            barWidth={22}
+            noOfSections={dummyData.graph_struct.data.length}
+            barBorderRadius={4}
+            frontColor="lightgray"
+            data={dummyData.graph_struct.data}
+            yAxisThickness={0}
+            xAxisThickness={0}
+            hideRules
+            showReferenceLine1
+            referenceLine1Position={threshold}
+            referenceLine1Config={{
+              color: "blue",
+              dashWidth: 2,
+              dashGap: 3,
+              thickness: 3,
+            }}
+          />
+        </View>
+        <View className="w-full bg-blue-700 rounded-lg p-4 mb-4">
+          <Text className="text-white text-xl font-bold">
+            Learn More About The Opposing Team
+          </Text>
+        </View>
       </View>
     </ScrollView>
   );
